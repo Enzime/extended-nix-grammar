4ee8e64cef2b5e051227c7dcf35e1b007a882ce8
Michael Hoang <enzime@users.noreply.github.com>
2022-01-17 22:41:00 +1100

    Add support for HTML inside Nix multiline strings

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+All notable changes to the "nix-ext" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [Unreleased]
+
+- Added injection grammar to extend Nix multiline strings to support language annotations
---
+++ examples/lang-ext-required.nix
@@ -0,0 +1,25 @@
+{
+  # TODO: yo
+  # REMOVEME: apple
+  help = ''html
+    REMOVEME
+    <a href="https://www.google.com">
+      G00G
+    </a><vscode>
+      aaa
+      aaa
+    </vscode>
+  so close'';
+  me = ''html
+    REMOVEME
+    <a href="https://www.google.com">
+      G00G
+    </a><vscode>
+    '''yet so far
+      aaa
+      aaa
+    </vscode>
+  '';
+  now = "homl";
+  thanks = "html";
+}
